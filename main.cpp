64c680f437cdf0c4b54224ae5b3587596fcbf59e
Matheus Bozzi <matheusabreubozzi@gmail.com>
2021-03-22 19:00:03 -0300

    finalizado. poucos bugs na pontuação, mas cansei

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@
 using namespace std;
 
 int keyStatus[256];
-static char pontos[32];
+static char pontos[100];
 void *font = GLUT_BITMAP_9_BY_15;
 int animate = 0;
 long int inicial = time(NULL);
@@ -33,6 +33,9 @@ Arena *arenaSVG = NULL;
 float facil = 0.5;
 float medio = 0.9;
 float dificil = 1.5;
+bool dificuldadeFacil = false;
+bool dificuldadeMedio = false;
+bool dificuldadeDificil = false;
 
 int xAntigo;
 int yAntigo;
@@ -43,6 +46,8 @@ bool flagSocoEsq = false;
 int contaSocoDirLutador = 0;
 int contaSocoEsqLutador = 0;
 
+int flag1SocoDirOp = 0;
+int flag1SocoEsqOp = 0;
 bool flagSocoDirOp = false;
 bool flagSocoEsqOp = false;
 int contaSocoDirOponente = 0;
@@ -76,8 +81,28 @@ void mouse(int botao, int estado, int x, int y)
 
 	if (botao == GLUT_LEFT_BUTTON)
 	{
-		cout << x << endl;
-		cout << y << endl;
+		// cout << x << endl;
+		// cout << y << endl;
+
+		if (x > 235 && x < 290 && y > 0 && y < 25)
+		{
+			dificuldadeFacil = true;
+			dificuldadeMedio = false;
+			dificuldadeDificil = false;
+		}
+		else if (x > 325 && x < 380 && y > 0 && y < 25)
+		{
+			dificuldadeMedio = true;
+			dificuldadeFacil = false;
+			dificuldadeDificil = false;
+		}
+		else if (x > 425 && x < 500 && y > 0 && y < 25)
+		{
+			dificuldadeMedio = false;
+			dificuldadeFacil = false;
+			dificuldadeDificil = true;
+		}
+
 		if (estado == GLUT_DOWN)
 		{
 			xAntigo = x;
@@ -117,32 +142,6 @@ void verificaSeAcertouSocoDireito(Point p, Oponente *o)
 	}
 }
 
-void verificaSeAcertouSocoDireitoOponente(Point p, Lutador *l)
-{
-
-	float odX = l->ObtemPosicao().x + arenaSVG->get_width() / 2;
-	float odY = l->ObtemPosicao().y + arenaSVG->get_height() / 2;
-
-	float dx = sqrt(pow(p.x - odX, 2) + pow(p.y - odY, 2));
-
-	if (dx >= lutadorOponente->ObtemRaio() / 2 + l->ObtemRaio())
-	{
-		// cout << "errou" << endl;
-		flagSocoDirOp = true;
-		flagSocoEsqOp = true;
-	}
-	else
-	{
-		if (flagSocoDirOp)
-		{
-			// cout << "ACERTOU" << endl;
-			contaSocoDirOponente += 1;
-			flagSocoDirOp = false;
-			flagSocoEsqOp = false;
-		}
-	}
-}
-
 void verificaSeAcertouSocoEsquerdo(Point p, Oponente *o)
 {
 
@@ -165,6 +164,31 @@ void verificaSeAcertouSocoEsquerdo(Point p, Oponente *o)
 	}
 }
 
+void verificaSeAcertouSocoDireitoOponente(Point p, Lutador *l)
+{
+
+	float odX = l->ObtemPosicao().x + arenaSVG->get_width() / 2;
+	float odY = l->ObtemPosicao().y + arenaSVG->get_height() / 2;
+
+	float dx = sqrt(pow(p.x - odX, 2) + pow(p.y - odY, 2));
+
+	if (dx >= lutadorOponente->ObtemRaio() / 2 + l->ObtemRaio())
+	{
+		flagSocoDirOp = true;
+		// flagSocoEsqOp = true;
+	}
+	else
+	{
+		if (flagSocoDirOp)
+		{
+			// cout << "ACERTOU" << endl;
+			contaSocoDirOponente += 1;
+			flagSocoDirOp = false;
+			flagSocoEsqOp = true;
+		}
+	}
+}
+
 void verificaSeAcertouSocoEsquerdoOponente(Point p, Lutador *l)
 {
 
@@ -176,7 +200,7 @@ void verificaSeAcertouSocoEsquerdoOponente(Point p, Lutador *l)
 	if (dx >= lutadorOponente->ObtemRaio() / 2 + l->ObtemRaio())
 	{
 		flagSocoEsqOp = true;
-		flagSocoDirOp = true;
+		// flagSocoDirOp = true;
 	}
 	else
 	{
@@ -184,7 +208,7 @@ void verificaSeAcertouSocoEsquerdoOponente(Point p, Lutador *l)
 		{
 			contaSocoEsqOponente += 1;
 			flagSocoEsqOp = false;
-			flagSocoDirOp = false;
+			flagSocoDirOp = true;
 		}
 	}
 }
@@ -200,7 +224,7 @@ void movimentoBraco(int x, int y)
 		if (x - xAntigo <= arenaSVG->get_width() / 2)
 		{
 			lutadorPrincipal->MudaTheta1(-45 + (x - xAntigo) * (135 / (arenaSVG->get_width() / 2)));
-			lutadorPrincipal->MudaTheta2(135 - (x - xAntigo) * (135 / (arenaSVG->get_width() / 2)));
+			lutadorPrincipal->MudaTheta2(135 - (x - xAntigo) * (110 / (arenaSVG->get_width() / 2)));
 			Point pSocoDir = lutadorPrincipal->verificaSocoDir(arenaSVG->get_width() / 2, arenaSVG->get_height() / 2, lutadorPrincipal->ObtemTheta1(), lutadorPrincipal->ObtemTheta2());
 			verificaSeAcertouSocoDireito(pSocoDir, lutadorOponente);
 		}
@@ -210,7 +234,7 @@ void movimentoBraco(int x, int y)
 		if ((x - xAntigo >= (-arenaSVG->get_height() / 2)))
 		{
 			lutadorPrincipal->MudaTheta3(-45 - (x - xAntigo) * (135 / (arenaSVG->get_height() / 2)));
-			lutadorPrincipal->MudaTheta4(135 + (x - xAntigo) * (135 / (arenaSVG->get_height() / 2)));
+			lutadorPrincipal->MudaTheta4(135 + (x - xAntigo) * (110 / (arenaSVG->get_height() / 2)));
 			Point pSocoEsq = lutadorPrincipal->verificaSocoEsq(arenaSVG->get_width() / 2, arenaSVG->get_height() / 2, -lutadorPrincipal->ObtemTheta3(), -lutadorPrincipal->ObtemTheta4());
 			verificaSeAcertouSocoEsquerdo(pSocoEsq, lutadorOponente);
 		}
@@ -322,160 +346,203 @@ bool dentroLutador(Lutador *lutadorPrincipal, Oponente *lutadorOponente)
 
 void idle(void)
 {
-
-	static GLdouble previousTime = 0;
-	GLdouble currentTime;
-	GLdouble timeDifference;
-	currentTime = glutGet(GLUT_ELAPSED_TIME);
-	timeDifference = currentTime - previousTime;
-	previousTime = currentTime;
-
-	bool wCheck;
-	bool aCheck;
-	bool sCheck;
-	bool dCheck;
-
-	dCheck = (keyStatus['D'] == 1 || keyStatus['d'] == 1);
-	aCheck = (keyStatus['A'] == 1 || keyStatus['a'] == 1);
-	wCheck = keyStatus['W'] == 1 || keyStatus['w'] == 1;
-	sCheck = keyStatus['S'] == 1 || keyStatus['s'] == 1;
-
-	Point dx = lutadorPrincipal->atualizaLutador(wCheck, sCheck, aCheck, dCheck, timeDifference);
-	Arena *a = arenaSVG;
-
-	lutadorPrincipal->MoveLutador(dx.x, 0);
-	bool estaDentro = dentroArenaLutador(lutadorPrincipal, a); //trata colisao com a arena
-	bool estaDentro2 = dentroOponente(lutadorPrincipal, lutadorOponente);
-	if (!estaDentro || !estaDentro2)
+	if ((dificuldadeFacil || dificuldadeMedio || dificuldadeDificil) && (!lutadorGanhou || !oponenteGanhou))
 	{
-		lutadorPrincipal->MoveLutador(-dx.x, 0);
-	}
+		static GLdouble previousTime = 0;
+		GLdouble currentTime;
+		GLdouble timeDifference;
+		currentTime = glutGet(GLUT_ELAPSED_TIME);
+		timeDifference = currentTime - previousTime;
+		previousTime = currentTime;
 
-	lutadorPrincipal->MoveLutador(0, dx.y);
-	estaDentro = dentroArenaLutador(lutadorPrincipal, a); //trata colisao com a arena
-	estaDentro2 = dentroOponente(lutadorPrincipal, lutadorOponente);
-	if (!estaDentro || !estaDentro2)
-	{
-		lutadorPrincipal->MoveLutador(0, -dx.y);
-	}
+		bool wCheck;
+		bool aCheck;
+		bool sCheck;
+		bool dCheck;
 
-	//MOVIMENTO DO OPONENTE
-	if (animate && !lutadorGanhou && !oponenteGanhou)
-	{
-		Point result = {lutadorOponente->ObtemPosicao().x - lutadorPrincipal->ObtemPosicao().x,
-						lutadorOponente->ObtemPosicao().y - lutadorPrincipal->ObtemPosicao().y};
-		double angle = atan2(result.y, result.x);
-		lutadorOponente->MudaAnguloJogador(-((180 - (angle * 180 / M_PI)) * 2) - (((angle * 180) / M_PI) - 90));
+		dCheck = (keyStatus['D'] == 1 || keyStatus['d'] == 1);
+		aCheck = (keyStatus['A'] == 1 || keyStatus['a'] == 1);
+		wCheck = keyStatus['W'] == 1 || keyStatus['w'] == 1;
+		sCheck = keyStatus['S'] == 1 || keyStatus['s'] == 1;
 
-		Point dy;
-		if (result.x > 0)
+		Point dx = lutadorPrincipal->atualizaLutador(wCheck, sCheck, aCheck, dCheck, timeDifference);
+		Arena *a = arenaSVG;
+
+		lutadorPrincipal->MoveLutador(dx.x, 0);
+		bool estaDentro = dentroArenaLutador(lutadorPrincipal, a); //trata colisao com a arena
+		bool estaDentro2 = dentroOponente(lutadorPrincipal, lutadorOponente);
+		if (!estaDentro || !estaDentro2)
 		{
-			dy = lutadorOponente->atualizaOponente(true, false, false, true, facil * timeDifference);
-		}
-		else if (result.x < 0)
-		{
-			dy = lutadorOponente->atualizaOponente(true, false, true, false, facil * timeDifference);
+			lutadorPrincipal->MoveLutador(-dx.x, 0);
 		}
 
-		lutadorOponente->MoveOponente(dy.x, 0);
-		bool estaDentroOp = dentroArenaOponente(lutadorOponente, a);
-		bool estaDentro2Op = dentroLutador(lutadorPrincipal, lutadorOponente);
-		if (!estaDentroOp || !estaDentro2Op)
+		lutadorPrincipal->MoveLutador(0, dx.y);
+		estaDentro = dentroArenaLutador(lutadorPrincipal, a); //trata colisao com a arena
+		estaDentro2 = dentroOponente(lutadorPrincipal, lutadorOponente);
+		if (!estaDentro || !estaDentro2)
 		{
-			lutadorOponente->MoveOponente(-dy.x, 0);
-			unsigned seed = time(0);
-			srand(seed);
-			int i = ((int)arenaSVG->get_width() / 2 * facil * 0.7) + rand() % ((int)arenaSVG->get_width() / 2);
-			if (i > ((int)arenaSVG->get_width() / 2))
+			lutadorPrincipal->MoveLutador(0, -dx.y);
+		}
+
+		//MOVIMENTO DO OPONENTE
+		if (animate && !lutadorGanhou && !oponenteGanhou)
+		{
+			Point result = {lutadorOponente->ObtemPosicao().x - lutadorPrincipal->ObtemPosicao().x,
+							lutadorOponente->ObtemPosicao().y - lutadorPrincipal->ObtemPosicao().y};
+			double angle = atan2(result.y, result.x);
+			lutadorOponente->MudaAnguloJogador(-((180 - (angle * 180 / M_PI)) * 2) - (((angle * 180) / M_PI) - 90));
+
+			float dificuldade, angulo;
+			if (dificuldadeFacil)
 			{
-				i = ((int)arenaSVG->get_width() / 2);
+				dificuldade = facil;
+				angulo = 0.9;
 			}
-			unsigned seed2 = time(0);
-			srand(seed);
-			int x = rand() % 2;
-			if (x % 2 == 0)
+			else if (dificuldadeMedio)
 			{
-				{
-					lutadorOponente->MudaTheta1(-45 + i * (135 / (arenaSVG->get_width() / 2)));
-					lutadorOponente->MudaTheta2(135 - i * (135 / (arenaSVG->get_width() / 2)));
-					Point pSocoDirOp = lutadorOponente->verificaSocoDirOponente(arenaSVG->get_width() / 2, arenaSVG->get_height() / 2, lutadorOponente->ObtemTheta1(), lutadorOponente->ObtemTheta2());
-					verificaSeAcertouSocoDireitoOponente(pSocoDirOp, lutadorPrincipal);
-					//tentar fazer um loop devagar
-				}
+				dificuldade = medio;
+				angulo = 0.85;
+			}
+			else if (dificuldadeDificil)
+			{
+				dificuldade = dificil;
+				angulo = 0.75;
+			}
+			Point dy;
+			if (result.x > 0)
+			{
+				dy = lutadorOponente->atualizaOponente(true, false, false, true, dificuldade * timeDifference);
+			}
+			else if (result.x < 0)
+			{
+				dy = lutadorOponente->atualizaOponente(true, false, true, false, dificuldade * timeDifference);
+			}
 
-				lutadorOponente->MudaTheta3(-45);
-				lutadorOponente->MudaTheta4(135);
+			lutadorOponente->MoveOponente(dy.x, 0);
+			bool estaDentroOp = dentroArenaOponente(lutadorOponente, a);
+			bool estaDentro2Op = dentroLutador(lutadorPrincipal, lutadorOponente);
+			if (!estaDentroOp || !estaDentro2Op)
+			{
+				lutadorOponente->MoveOponente(-dy.x, 0);
+				unsigned seed = time(0);
+				srand(seed);
+				int i = ((int)arenaSVG->get_width() / 2 * dificuldade * 0.7) + rand() % ((int)arenaSVG->get_width() / 2);
+				if (i > ((int)arenaSVG->get_width() / 2))
+				{
+					i = ((int)arenaSVG->get_width() / 2);
+				}
+				unsigned seed2 = time(0);
+				srand(seed);
+				int x = rand() % 2;
+				if (x % 2 == 0)
+				{
+					{
+						lutadorOponente->MudaTheta1(-45 + i * (135 / (arenaSVG->get_width() / 2)));
+						lutadorOponente->MudaTheta2(135 - i * (angulo * 135 / (arenaSVG->get_width() / 2)));
+						Point pSocoDirOp = lutadorOponente->verificaSocoDirOponente(arenaSVG->get_width() / 2, arenaSVG->get_height() / 2, lutadorOponente->ObtemTheta1(), lutadorOponente->ObtemTheta2());
+						if(!flag1SocoDirOp){
+							pSocoDirOp = {0,0};
+							flag1SocoDirOp++;
+						}
+						verificaSeAcertouSocoDireitoOponente(pSocoDirOp, lutadorPrincipal);
+						//tentar fazer um loop devagar
+					}
+					lutadorOponente->MudaTheta3(-45);
+					lutadorOponente->MudaTheta4(135);
+				}
+				else
+				{
+					lutadorOponente->MudaTheta1(-45);
+					lutadorOponente->MudaTheta2(135);
+					{
+						lutadorOponente->MudaTheta3(-45 + i * (135 / (arenaSVG->get_height() / 2)));
+						lutadorOponente->MudaTheta4(135 - i * (angulo * 135 / (arenaSVG->get_height() / 2)));
+						Point pSocoEsqOp = lutadorOponente->verificaSocoEsqOponente(arenaSVG->get_width() / 2, arenaSVG->get_height() / 2, -lutadorOponente->ObtemTheta3(), -lutadorOponente->ObtemTheta4());
+						if(!flag1SocoEsqOp){
+							pSocoEsqOp = {0,0};
+							flag1SocoEsqOp++;
+						}
+						verificaSeAcertouSocoEsquerdoOponente(pSocoEsqOp, lutadorPrincipal);
+						//voltar devagar
+					}
+				}
 			}
 			else
 			{
 				lutadorOponente->MudaTheta1(-45);
 				lutadorOponente->MudaTheta2(135);
-				{
-					lutadorOponente->MudaTheta3(-45 + i * (135 / (arenaSVG->get_height() / 2)));
-					lutadorOponente->MudaTheta4(135 - i * (135 / (arenaSVG->get_height() / 2)));
-					Point pSocoEsqOp = lutadorOponente->verificaSocoEsqOponente(arenaSVG->get_width() / 2, arenaSVG->get_height() / 2, -lutadorOponente->ObtemTheta3(), -lutadorOponente->ObtemTheta4());
-					verificaSeAcertouSocoEsquerdoOponente(pSocoEsqOp, lutadorPrincipal);
-					//voltar devagar
-				}
+				lutadorOponente->MudaTheta3(-45);
+				lutadorOponente->MudaTheta4(135);
+			}
+
+			lutadorOponente->MoveOponente(0, dy.y);
+			estaDentro = dentroArenaOponente(lutadorOponente, a);
+			estaDentro2 = dentroOponente(lutadorPrincipal, lutadorOponente);
+			if (!estaDentro || !estaDentro2)
+			{
+				lutadorOponente->MoveOponente(0, -dy.y);
 			}
 		}
-		else
-		{
-			lutadorOponente->MudaTheta1(-45);
-			lutadorOponente->MudaTheta2(135);
-			lutadorOponente->MudaTheta3(-45);
-			lutadorOponente->MudaTheta4(135);
-		}
 
-		lutadorOponente->MoveOponente(0, dy.y);
-		estaDentro = dentroArenaOponente(lutadorOponente, a);
-		estaDentro2 = dentroOponente(lutadorPrincipal, lutadorOponente);
-		if (!estaDentro || !estaDentro2)
-		{
-			lutadorOponente->MoveOponente(0, -dy.y);
-		}
+		// glutTimerFunc(1000 / 60, idle, 0);
 	}
 
-	// glutTimerFunc(1000 / 60, idle, 0);
 	glutPostRedisplay();
 }
 
 void printPontuacao()
 {
-	if (contaSocoDirLutador + contaSocoEsqLutador >= 10)
+	if (dificuldadeFacil || dificuldadeMedio || dificuldadeDificil)
 	{
-		lutadorGanhou = true;
-		char *pontuacao;
-		sprintf(pontos, "PARABENS, VOCE GANHOU! :D");
-		glColor3f(0.0, 0.0, 0.0);
-		glRasterPos2f(-(arenaSVG->get_width() / 2) + 5, -(arenaSVG->get_height() / 2) + 10);
-
-		pontuacao = pontos;
-		while (*pontuacao)
+		if (contaSocoDirLutador + contaSocoEsqLutador >= 10)
 		{
-			glutBitmapCharacter(font, *pontuacao);
-			pontuacao++;
+			lutadorGanhou = true;
+			char *pontuacao;
+			sprintf(pontos, "PARABENS, VOCE GANHOU! :D");
+			glColor3f(0.0, 0.0, 0.0);
+			glRasterPos2f(-(arenaSVG->get_width() / 2) + 5, -(arenaSVG->get_height() / 2) + 10);
+
+			pontuacao = pontos;
+			while (*pontuacao)
+			{
+				glutBitmapCharacter(font, *pontuacao);
+				pontuacao++;
+			}
 		}
-	}
-	else if (contaSocoDirOponente + contaSocoEsqOponente >= 10)
-	{
-		oponenteGanhou = true;
-		char *pontuacao;
-		sprintf(pontos, "VOCE PERDEU, TENTE NOVAMENTE!");
-		glColor3f(0.0, 0.0, 0.0);
-		glRasterPos2f(-(arenaSVG->get_width() / 2) + 5, -(arenaSVG->get_height() / 2) + 10);
-
-		pontuacao = pontos;
-		while (*pontuacao)
+		else if (contaSocoDirOponente + contaSocoEsqOponente >= 10)
 		{
-			glutBitmapCharacter(font, *pontuacao);
-			pontuacao++;
+			oponenteGanhou = true;
+			char *pontuacao;
+			sprintf(pontos, "VOCE PERDEU, TENTE NOVAMENTE!");
+			glColor3f(0.0, 0.0, 0.0);
+			glRasterPos2f(-(arenaSVG->get_width() / 2) + 5, -(arenaSVG->get_height() / 2) + 10);
+
+			pontuacao = pontos;
+			while (*pontuacao)
+			{
+				glutBitmapCharacter(font, *pontuacao);
+				pontuacao++;
+			}
+		}
+		else
+		{
+			char *pontuacao;
+			sprintf(pontos, "Lutador: %2d x %2d Oponente", contaSocoDirLutador + contaSocoEsqLutador, contaSocoDirOponente + contaSocoEsqOponente);
+			glColor3f(0.0, 0.0, 0.0);
+			glRasterPos2f(-(arenaSVG->get_width() / 2) + 5, -(arenaSVG->get_height() / 2) + 10);
+
+			pontuacao = pontos;
+			while (*pontuacao)
+			{
+				glutBitmapCharacter(font, *pontuacao);
+				pontuacao++;
+			}
 		}
 	}
 	else
 	{
 		char *pontuacao;
-		sprintf(pontos, "Lutador: %2d x %2d Oponente", contaSocoDirLutador + contaSocoEsqLutador, contaSocoDirOponente + contaSocoEsqOponente);
+		sprintf(pontos, "CLIQUE NA DIFICULDADE:    FACIL     MEDIO      DIFICIL");
 		glColor3f(0.0, 0.0, 0.0);
 		glRasterPos2f(-(arenaSVG->get_width() / 2) + 5, -(arenaSVG->get_height() / 2) + 10);
 
